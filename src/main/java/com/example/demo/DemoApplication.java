846c98d215483ed665b729b89de2bb3a218a28af
Le Hu <le.hu@CNle.hu>
2019-11-15 12:41:40 +0800

    add gitignore

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,26 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        String filePath = "/Users/le.hu/workspace/jianshu/spring-cloud-yaml-demo/out/production/classes";
+        URL[] urls = new URL[1];
+        try (URLClassLoader classLoader = new URLClassLoader(urls);) {
+            URL url = new URL("file", null, filePath);
+            urls[0] = url;
+            Class<?> contractController = classLoader.loadClass("com.example.demo.ContractController");
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
